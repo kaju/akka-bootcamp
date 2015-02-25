b3ac1b3b64459d60ed5327e5127dff9bf1add174
Simon Laroche <laroche.simon@gmail.com>
2015-02-24 21:14:54 -0500

    Unit 1.2

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -10,6 +10,7 @@ namespace WinTail
     class ConsoleReaderActor : UntypedActor
     {
         public const string ExitCommand = "exit";
+        public const string StartCommand = "start";
         private IActorRef _consoleWriterActor;
 
         public ConsoleReaderActor(IActorRef consoleWriterActor)
@@ -19,21 +20,59 @@ namespace WinTail
 
         protected override void OnReceive(object message)
         {
-            var read = Console.ReadLine();
-            if (!string.IsNullOrEmpty(read) && String.Equals(read, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            if (message.Equals(StartCommand))
             {
-                // shut down the system (acquire handle to system via
-                // this actors context)
-                Context.System.Shutdown();
-                return;
+                DoPrintInstructions();
             }
-
-            // send input to the console writer to process and print
-            _consoleWriterActor.Tell(read);
-
+            else if (message is Messages.InputError)
+            {
+                _consoleWriterActor.Tell(message as Messages.InputError);
+            }
+            GetAndValidateInput();
+            
             // continue reading messages from the console
-            Self.Tell("continue");
+            Self.Tell(new Messages.ContinueProcessing());
         }
 
+        private void GetAndValidateInput()
+        {
+            var message = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(message))
+            {
+                Self.Tell(new Messages.NullInputError("No input recieved."));
+            }
+            else if (message.Equals(ExitCommand))
+            {
+                Context.System.Shutdown();
+            }
+            else
+            {
+                var valid = IsVaid(message);
+
+                if (valid)
+                {
+                    _consoleWriterActor.Tell(new Messages.InputSuccess("Thank you, message was valid."));
+                    
+                }
+                else
+                {
+                    _consoleWriterActor.Tell(new Messages.InputError("Invalid: input had an odd number of characters."));
+                }
+                Self.Tell(new Messages.ContinueProcessing());
+            }
+        }
+
+        private bool IsVaid(string message)
+        {
+            return message.Length % 2 == 0;
+        }
+
+        private void DoPrintInstructions()
+        {
+            Console.WriteLine("Write whatever you want into the console!");
+            Console.WriteLine("Some entries will pass validation, and some won't...\n\n");
+            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
+        }
     }
 }
---
+++ src/Unit-1/DoThis/Messages.cs
@@ -0,0 +1,60 @@
+﻿namespace WinTail
+{
+    public class Messages
+    {
+        #region Neutral / System messages
+
+        public class ContinueProcessing
+        {
+
+        }
+
+        #endregion
+
+        #region Success messages
+
+        public class InputSuccess
+        {
+            public string Reason { get; private set; }
+
+            public InputSuccess(string reason)
+            {
+                this.Reason = reason;
+            }
+        }
+
+        #endregion
+
+        #region Failure messages
+
+        public class InputError
+        {
+            public string Reason { get; private set; }
+
+            public InputError(string reason)
+            {
+                this.Reason = reason;
+            }
+        }
+
+        public class NullInputError : InputError
+        {
+            public NullInputError(string reason)
+                : base(reason)
+            {
+            }
+        }
+
+        public class ValidationInputError : InputError
+        {
+            public ValidationInputError(string reason)
+                : base(reason)
+            {
+            }
+        }
+
+
+        #endregion
+
+    }
+}
